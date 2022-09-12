bbe0be5194d6519d4ad7bdfd6bf4fe71f1d4e7c9
changnam <=>
2022-09-12 20:07:06 +0900

    posts api 에 대해서, get, post request 방식 적용

--- src/api.js
+++ src/api.js
@@ -11,9 +11,51 @@ async function postRequest(){
 		params: {'format':'json'}
 	};
 
-	let res = await axios.post('http://localhost:8080/ask', xmlBodyStr, config);
+	try {
+		let res = await axios.post('http://localhost:8080/ask', xmlBodyStr, config);
 	
-	console.log(res.data);
+		console.log(res.data);
+	} catch(error){
+		console.error(error);
+	}
+	
 }	
 
+
+async function postPosts(){ 
+	var bodyStr = {'title':'axios post 등록','content':'axios post 내용입니다','writer':'고창남','noticeYn':false,'deleteYn':false};
+	
+	var config = {
+		headers: {'Content-Type': 'application/json'},
+		params: {'format':'json'}
+	};
+
+	try {
+		let res = await axios.post('http://localhost:8080/api/posts', bodyStr, config);
+	
+		console.log(res.data);
+	} catch(error){
+		console.error(error);
+	}
+	
+}	
+
+
+async function getRequest(){
+	var config = {
+		params: {}
+	}
+	try {
+		//let res = await axios.get('https://springbootbase.honsoft.co.kr/api/v1/foo');
+		//let res = await axios.get('http://springbootbase.honsoft.co.kr/xframe5/sample/selectView');
+		let res = await axios.get('http://localhost:8080/api/posts/1');
+		console.log(res.data);
+	} catch (error){
+		console.error(error);
+	}
+
+}
+
 postRequest();
+postPosts();
+getRequest();

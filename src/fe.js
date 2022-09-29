092b2de20e6b0001662c6580406f1ad03209a9fe
changnam <=>
2022-09-29 21:17:32 +0900

    file explorer js

---
+++ src/fe.js
@@ -0,0 +1,24 @@
+function walkSync(currentDirPath, callback){
+    var fs = require('fs');
+    path = require('path');
+
+    fs.readdirSync(currentDirPath).forEach(function(name){
+        var filePath = path.join(currentDirPath,name);
+        var stat = fs.statSync(filePath);
+        if(stat.isFile()){
+            callback(filePath, stat);
+
+        } else if (stat.isDirectory()){
+            walkSync(filePath,callback);
+        }
+    })
+}
+
+
+if (process.argv.length === 3){
+    const filename = process.argv[2];
+
+    walkSync(filename, function(filePath, stat){
+        console.log(filePath);
+    });
+}

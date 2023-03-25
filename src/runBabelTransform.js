b35ed70ca6d2025aada3d635a3d31d8413a37fce
Changnam Go <changnam.go@gmail.com>
2023-03-25 13:06:58 +0900

    global count 추가, CCNConst.cntStep 으로 호출해야 함

--- src/runBabelTransform.js
+++ src/runBabelTransform.js
@@ -35,7 +35,7 @@ if (process.argv.length === 3) {
 						t.callExpression(
 							t.memberExpression(t.identifier('factory'), t.identifier('consoleprint')),
 							//[t.stringLiteral(filename+": "+path.node.id.name+ " started. ")]
-							[t.binaryExpression('+',t.updateExpression('++',t.identifier('cntStep')), t.stringLiteral(filename+": "+path.node.id.name+ " started."))]
+							[t.binaryExpression('+',t.updateExpression('++',t.memberExpression(t.identifier('CCNConst'),t.identifier('cntStep')),true), t.stringLiteral("|-|"+filename+"|-|"+path.node.id.name+ "|-| started."))]
 							//[t.stringLiteral(filename+": "+path.node.id.name+ " started. caller : +"+path.node.id.name+".caller.toString().substring(1,30))")]
 						)
 					);

fa60e4c2a4e84f476f8678dda21a71bb7af9f409
Changnam Go <changnam.go@gmail.com>
2024-05-05 11:30:47 +0900

    parse 테스트를 위한 simple 소스를 파일에 backtick으로 추가

--- src/parser.js
+++ src/parser.js
@@ -2,12 +2,18 @@ const fs = require('fs');
 const babel = require('@babel/core');
 const syntaxJsx = require('@babel/plugin-syntax-jsx')
 
+var src = `var a = 1; // pathA, path.key = 0
+var b = 2; // pathB, path.key = 1
+var c = 3; // pathC, path.key = 2
+`;
+
 if (process.argv.length === 3){
 	const filename = process.argv[2];
 	console.log(filename);
 	
 	const source = fs.readFileSync(filename).toString();
-	console.log(source);
+	//console.log(source);
+	
 	const ast = babel.parseSync(source, {
 		babelrc: false,
 		configFile: false,
